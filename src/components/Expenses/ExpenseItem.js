5e69453890e0c8c0526d0d03f364bcc67fe3d564
Prathima <kotianpriya64@gmail.com>
2022-02-28 16:54:34 +0530

    updated files

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,63 @@
+import React ,{useState} from 'react';
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css';
+// function ExpenseItem(props)
+// {
+//     const [title,setTitle]=useState(props.title);
+//     // console.log('Expense item evualted by recat');
+//      const ClickHandler=()=>
+//      {
+//          setTitle('updated')
+//          console.log('title updated')
+//      }
+     
+//     return(
+//         <li>
+//         <div className="expense-item">
+//             <ExpenseDate date={props.date} />
+//             <div className="expense-item_decription">
+//                 <h1>{title}</h1>
+//                 <div className="expense-item_price">${props.amount}</div>
+//             </div>
+//             <button onClick={ClickHandler}>Change Title</button>
+//         </div>
+//         </li>
+//     );
+// }
+
+// export default ExpenseItem;
+
+
+
+
+
+
+
+
+
+
+const ExpenseItem=(props)=>
+{
+    const [title,setTitle]=useState(props.title)
+    const clickHandler=()=>
+    {
+        setTitle("Clicked")
+    }
+    
+    return(
+        <li>
+        <div className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item_description'><h2>{title}</h2></div>
+            <div className='expense-item_price'>${props.amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
+        </div>
+        </li>
+    )
+
+}
+export default ExpenseItem
+
+
+
+
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from "./ExpenseItem"
 import './ExpensesList.css'
 
 const ExpensesList=props=>
