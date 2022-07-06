1e8db5931c808b5375b301126edcfda46072ec62
Kavya1234Shree <kavyashree043@gmail.com>
2022-07-06 09:55:54 +0530

    updated one

--- src/React Learning/Components/Examples/Covid19/components/CountryPicker/CountryPicker.js
+++ src/React Learning/Components/Examples/Covid19/components/CountryPicker/CountryPicker.js
@@ -1,26 +1,26 @@
-// import { FormControl, NativeSelect } from '@mui/material'
-// import React, { useEffect, useState } from 'react'
-// import {  fetchCountries } from '../../api'
-// import styles from './CountryPicker.module.css'
+import { FormControl, NativeSelect } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import {  fetchCountries } from '../../api'
+import styles from './CountryPicker.module.css'
 
-// const CountryPicker = ({handleCountryChange}) => {
-//   const [fetchedCountries,setFetchedCountries]=useState([])
-//   useEffect(() => {
-//     const fetchAPI = async () => {
-//       setFetchedCountries(await fetchCountries());
-//     };
+const CountryPicker = ({handleCountryChange}) => {
+  const [fetchedCountries,setFetchedCountries]=useState([])
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
 
-//     fetchAPI();
-//   }, [setFetchedCountries]);
-//   console.log(fetchedCountries)
-//   return (
-//     <FormControl className={styles.formControl}>
-//       <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
-//        <option value='global'>United States</option>
-//        {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-//       </NativeSelect>
-//     </FormControl>
-//   )
-// }
+    fetchAPI();
+  }, [setFetchedCountries]);
+  console.log(fetchedCountries)
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+       <option value='global'>United States</option>
+       {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+      </NativeSelect>
+    </FormControl>
+  )
+}
 
-// export default CountryPicker
+export default CountryPicker
