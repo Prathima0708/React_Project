a5b758ae9e9e4c30cb98acce3fc96b08a5ddc018
Prathima0708 <kotianpriya64@gmail.com>
2022-07-13 11:27:03 +0530

    updated one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-
-import { fetchData } from "./React Learning/Components/Examples/Covid19/api";
-import styles from "./React Learning/Components/Examples/Covid19/App.module.css";
-import Cards from "./React Learning/Components/Examples/Covid19/components/Cards/Cards";
-import Chart from "./React Learning/Components/Examples/Covid19/components/Chart/Chart";
-import CountryPicker from "./React Learning/Components/Examples/Covid19/components/CountryPicker/CountryPicker";
-=======
-
 import React, { useEffect, useState } from "react";
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import AddContact from "./React Learning/Components/Examples/ContactManager/components/AddContact";
 import ContactList from "./React Learning/Components/Examples/ContactManager/components/ContactList";
 
 import Header from "./React Learning/Components/Examples/ContactManager/components/Header";
-
-
-
->>>>>>> 978a607443fde9b8a5c16f85e87382ed38ad4896
 
 // function App() {
 //   return (
@@ -65,24 +51,6 @@ import Header from "./React Learning/Components/Examples/ContactManager/componen
 
 // export default App;
 
-<<<<<<< HEAD
-class App extends React.Component {
-  state = {
-    data: {},
-    country: "",
-  };
-
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
-  }
-
-  handleCountryChange = async (country) => {
-    console.log(country);
-    const data = await fetchData(country);
-=======
-
-
 // class App extends React.Component {
 //   state={
 //     data:{},
@@ -95,31 +63,14 @@ class App extends React.Component {
 // }
 
 // handleCountryChange = async (country) => {
- 
-//   const fetchedData = await fetchData(country);
 
+//   const fetchedData = await fetchData(country);
 
 //   this.setState({ data:fetchedData,country:country });
 // }
 
->>>>>>> 978a607443fde9b8a5c16f85e87382ed38ad4896
+// this.setState({ data, country: country });
 
-    // this.setState({ data, country: country });
-  };
-
-<<<<<<< HEAD
-  render() {
-    const { data } = this.state;
-
-    return (
-      <div className={styles.container}>
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
-      </div>
-    );
-  }
-=======
 //   render() {
 //     const {data,country}=this.state
 
@@ -128,7 +79,7 @@ class App extends React.Component {
 //         <Cards data={data}/>
 //         <CountryPicker handleCountryChange={this.handleCountryChange}/>
 //         <Chart data={data} country={country}/>
-      
+
 //       </div>
 //     );
 //   }
@@ -136,53 +87,51 @@ class App extends React.Component {
 
 // export default App;
 
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([
+      ...contacts,
+      { id: Math.floor(Math.random() * 100), ...contact },
+    ]);
+  };
 
-function App(){
-  const LOCAL_STORAGE_KEY="contacts"
-const [contacts,setContacts]=useState([])
-const addContactHandler=(contact)=>{
-  console.log(contact)
-  setContacts([...contacts,{id:Math. floor(Math. random() * 100),...contact}])
-} 
+  const deleteContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  };
 
-const deleteContactHandler=(id)=>{
-const newContactList=contacts.filter((contact)=>{
-  return contact.id !==id
-})
-setContacts(newContactList)
->>>>>>> 978a607443fde9b8a5c16f85e87382ed38ad4896
-}
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
 
-    useEffect(()=>{
-const retrieveContacts=JSON.parse( localStorage.getItem(LOCAL_STORAGE_KEY))
-if (retrieveContacts)setContacts(retrieveContacts)
-    },[])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-    useEffect(()=>{
-      localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts))
-          },[contacts])
-
-  return(
+  return (
     <>
- 
+      {/* <Dashboard /> */}
+      {/* <Book/> */}
 
-  {/* <Dashboard /> */}
-  {/* <Book/> */}
-
- 
-
-       
-          {/* <Route  path="/users/add" element={AddUser} />
+      {/* <Route  path="/users/add" element={AddUser} />
           <Route  path="/users/edit/:id" element={EditUser} />
           <Route  path="/users/:id" element={User} />
           <Route element={NotFound} /> */}
-          <div className="container">
- <Header/>
-<AddContact addContactHandler={addContactHandler}/>
-<ContactList contacts={contacts} getContactId={deleteContactHandler}/>
-</div>
-  </>
-  )
+      <div className="container">
+        <Header />
+        <AddContact addContactHandler={addContactHandler} />
+        <ContactList contacts={contacts} getContactId={deleteContactHandler} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
